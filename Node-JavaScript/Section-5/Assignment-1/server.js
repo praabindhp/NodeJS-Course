7cf1a50efb109c447654a36b2a15d004dcfa2d61
praabindhp <praabindhp@gmail.com>
2024-02-27 16:34:09 +0530

    NodeJS Express Server

--- Node-JavaScript/Section-5/Assignment-1/server.js
+++ Node-JavaScript/Section-5/Assignment-1/server.js
@@ -4,15 +4,6 @@ const bodyParser = require('body-parser')
 const app = express()
 app.use(bodyParser.urlencoded({ extended: false }))
 
-app.use('/add-product', (req, res, next) => {
-    res.send('<form action="/product" method="POST"><input type="text" name="title"><button type="submit">Add Product</button></form>')
-})
-
-app.post('/product', (req, res, next) => {
-    console.log(req.body)
-    res.redirect('/')
-})
-
 app.use('/users', (req, res, next) => {
     console.log('Howdy, Welcome User !')
     res.send('<h1>Howdy, Welcome User !</h1>')
